ea824fb566b83358b800d01750749e91af789cb4
alexs <alexs@192.168.72.113>
2017-08-09 13:38:54 -0300

    Versão inicial

---
+++ MyCRM/src/com/yourcodelab/db/ConnectDB.java
@@ -0,0 +1,34 @@
+package com.yourcodelab.db;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectDB {
+	private Connection con;
+	
+	private String driver;
+	private String url;
+	private String username;
+	private String password;
+	
+	/**
+	 * Classe de conexão com Banco de Dados
+	 * 
+	 */
+	public ConnectDB(){
+		// Melhoria futura: colocar essas informações em arquivo *.properties
+		driver = "com.mysql.jdbc.Driver";
+		url = "jdbc:mysql://localhost:3306/your_jdbc";
+		username = "root";
+		password = "";
+	}
+	
+	public Connection getConnection() throws IOException, ClassNotFoundException, SQLException{
+		Class.forName(driver) ;
+		this.con = DriverManager.getConnection(url, username, password);
+		
+		return this.con;
+	}
+}
